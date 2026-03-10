cbde8871dd60f9fb6658d816873444ea7a3cfc26
Александра Сычева <sashasychevaisip@gmail.com>
2026-03-10 14:42:41 +0500

    Создание формы FormProducts

--- FormLogin.Designer.cs
+++ FormLogin.Designer.cs
@@ -77,6 +77,7 @@
             btnGuest.TabIndex = 5;
             btnGuest.Text = "Войти как гость";
             btnGuest.UseVisualStyleBackColor = false;
+            btnGuest.Click += BtnGuest_Click;
             // 
             // btnLogin
             // 
@@ -89,6 +90,7 @@
             btnLogin.TabIndex = 4;
             btnLogin.Text = "Войти";
             btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += BtnLogin_Click;
             // 
             // txtPassword
             // 
--- FormLogin.cs
+++ FormLogin.cs
@@ -1,10 +1,53 @@
+using shoeProject.Models;
+
 namespace shoeProject
 {
     public partial class FormLogin : Form
     {
+        public User CurrentUser { get; private set; }
+        public bool IsGuest { get; private set; }
+
         public FormLogin()
         {
             InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtLogin.Text) || String.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                MessageBox.Show("Введите логин и пароль", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (var db = new ShopContext())
+            {
+                var user = db.Users
+                    .Where(w => w.Login == txtLogin.Text && w.Pass == txtPassword.Text)
+                    .FirstOrDefault();
+
+                if (user != null)
+                {
+                    CurrentUser = user;
+                    IsGuest = false;
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void BtnGuest_Click(object sender, EventArgs e)
+        {
+            CurrentUser = null;
+            IsGuest=true;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
         }
     }
 }
