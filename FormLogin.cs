0730be56f6324fc139aa8224236b5498272b7bc8
Александра Сычева <sashasychevaisip@gmail.com>
2026-03-10 13:44:00 +0500

    Создание формы FormLogin

--- FormLogin.cs
+++ FormLogin.cs
@@ -1,8 +1,8 @@
 namespace shoeProject
 {
-    public partial class Form1 : Form
+    public partial class FormLogin : Form
     {
-        public Form1()
+        public FormLogin()
         {
             InitializeComponent();
         }
